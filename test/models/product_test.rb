420929f7b0d1fc1337764bffe1f7497f31569bb5
Nigel Lowry <nigel@lemmata.co.uk>
2019-04-24 21:31:41 +0100

    Add (extra) length test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -60,4 +60,19 @@ class ProductTest < ActiveSupport::TestCase
     assert product.invalid?
     assert_equal ["has already been taken"], product.errors[:title]
   end
+
+  test "title must be at least 10 characters" do
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy",
+                          price:       1,
+                          image_url:   "zzz.jpg")
+
+    product.title = 'a' * 9
+    assert product.invalid?
+    assert_equal ["is too short (minimum is 10 characters)"],
+      product.errors[:title]
+
+    product.title = 'a' * 10
+    assert product.valid?
+  end
 end
