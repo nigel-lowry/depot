3ae5846cb17ac12453d8521f8afce2fb84648b34
Nigel Lowry <nigel@lemmata.co.uk>
2019-04-25 10:11:59 +0100

    Show Web console when using Docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Enable the web console to be seen by all (helpful with Docker)
+  config.web_console.whitelisted_ips = %w( 0.0.0.0/0 ::/0 )
 end
