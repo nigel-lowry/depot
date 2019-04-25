c7a354a3f865623db399205edfee71ba1c3cb8b5
Nigel Lowry <nigel@lemmata.co.uk>
2019-04-25 19:52:48 +0100

    Group line items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190425182414) do
+ActiveRecord::Schema.define(version: 20190425183617) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
