1b0bbeffab95cf314c75c4dff79fea5358c42d4b
Nigel Lowry <nigel@lemmata.co.uk>
2019-04-27 13:49:54 +0100

    Now have React selection box

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -77,6 +77,18 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
+
+    def pay_type_params
+      if order_params[:pay_type] == "Credit Card"
+	params.require(:order).permit(:credit_card_number, :expiration_date)
+      elsif order_params[:pay_type] == "Check"
+        params.require(:order).permit(:routing_number, :account_number)
+      elsif order_params[:pay_type] == "Purchase Order"
+	params.require(:order).permit(:po_number)
+      else
+        {}
+      end
+    end
   
     def ensure_cart_isnt_empty
       if @cart.line_items.empty?
